e23a44ea2a1955c923a6ad21d49f4b2a05d6a843
Вадим Лукичев <vadim.lukichyov@ligastavok.ru>
2020-04-13 13:34:17 +0300

    minor update

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 console.log('Start');
 
 var mysql = require('mysql2');
-var term = require( 'terminal-kit' ).terminal ;
+var term = require('terminal-kit').terminal;
 
+var databaseName = 'progress';
 const mysqlConnection = mysql.createConnection({
     host: "v392persei.com",
     port: 13306,
     user: "progress",
     password: "Progress_1169",
-  });
+});
 
-mysqlConnection.connect(function(err) {
+mysqlConnection.connect(function (err) {
     if (err) throw err;
     console.log("База данных MySql подключена");
 
@@ -23,14 +24,13 @@ mysqlConnection.connect(function(err) {
             var table1Str3 = workbook.sheet("Лист1").range("A13:GS13").value();
 
             var table1 = [];
-            
+
             table1Str2.forEach(element => {
 
                 //console.log(element)
             });
 
-            for (var i = 0; i <= 200; i++)
-            {
+            for (var i = 0; i <= 200; i++) {
                 table1[i] = JSON.stringify(
                     {
                         id: table1Str1[0][i],
@@ -39,33 +39,29 @@ mysqlConnection.connect(function(err) {
                     }
                 );
             }
-    
+
             //console.log(value);
             loading(table1);
         });
 });
 
 //Реализация загрузки
-function loading(table1)
-{
+function loading(table1) {
     mysqlConnection.query(
-        'select * from progress.table1',
-        function(err, results, fields)
-        {
+        `select * from ${databaseName}.table1`,
+        function (err, results, fields) {
             results.forEach(element => {
                 if (element['id'] == "2") return;
                 var array = Object.entries(element);
-                
+
                 var count = 0;
                 term.green("Название:   ");
                 term.green("Обозначeние:     ");
                 term.green("Состояние/Время:                                  ");
                 term.green("Описание: \n");
 
-                for (var i = 0; i <= 200; i++)
-                {
-                    if (array[0][i] != "id")
-                    {
+                for (var i = 0; i <= 200; i++) {
+                    if (array[0][i] != "id") {
                         var field = JSON.parse(table1[i]);
 
                         var reg = /item/;
@@ -74,62 +70,67 @@ function loading(table1)
 
                         var line2 = "";
                         var line3 = "";
-                        
 
-                        for (var j = 0; j < 17 - String(field.name).length ; j++)
-                        {
+
+                        for (var j = 0; j < 17 - String(field.name).length; j++) {
                             line2 += " ";
                         }
 
-                        for (var j = 0; j < 50 - String(array[i][1]).length ; j++)
-                        {
+                        for (var j = 0; j < 50 - String(array[i][1]).length; j++) {
                             line3 += " ";
                         }
 
                         term.white(array[i][0] + line);
-                        term.white(((field.name != undefined) ? field.name : "undefined")  + line2);
-                        term.white(array[i][1]  + line3);
+                        term.white(((field.name != undefined) ? field.name : "undefined") + line2);
+                        term.white(array[i][1] + line3);
                         console.log("Описание: " + field.description + '\n');
-                    } 
-                    if (i % 2 == 0)
-                    {
+                    }
+                    if (i % 2 == 0) {
                         term('\n');
                     }
                 }
 
-                var nameTable;
+                var nameTable = 'table2';
                 var nameElement;
                 var state;
 
-                term.magenta( "Введите название таблицы:  \n" ) ;
+                // term.magenta("Введите название таблицы:  \n");
+                // term.inputField(
+                //     function (error, input) {
+                //         nameTable = input;
+
+                term.magenta("\nВведите название элемента:  \n");
                 term.inputField(
-                    function( error , input ) {
-                        nameTable = input;
+                    function (error, input) {
+                        nameElement = input;
 
-                        term.magenta( "\nВведите название элемента:  \n" ) ;
+                        term.magenta("\nВведите новое значение для элемента: ");
                         term.inputField(
-                            function( error , input ) {
-                                nameElement = input;
-
-                                term.magenta( "\nВведите состояние (1 или 0): " ) ;
-                                term.inputField(
-                                    function( error , input ) {
-                                        state = input;
-                                        term.green("\n" + nameTable + " " + nameElement + " " + state);
+                            function (error, input) {
+                                state = input;
+                                //term.green("\n" + nameTable + " " + nameElement + " " + state);
+                                term('\n');
+                                var queryString = `UPDATE ${databaseName}.${nameTable} SET ${nameElement}=${state} WHERE id=1`;
+                                term.green(queryString);
+                                mysqlConnection.query(queryString,
+                                    function (err, results, fields) {
+                                        console.log(err);
+                                        console.log(results);
                                         process.exit();
-                                    }
-                                );
+                                    });
                             }
                         );
                     }
                 );
+                //}
+                //);
 
-                
 
-                
 
-                
-                
+
+
+
+
             });
         }
     )
